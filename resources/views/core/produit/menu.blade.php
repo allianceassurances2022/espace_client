5cfc4d998d5cc9d463199333fa5b4361382372ff
AMINA BELMESSOUS <ABELMESSOUS@allianceassurances.com>
2020-07-15 09:05:00 +0100

    Initial commit

--- resources/views/core/produit/menu.blade.php
+++ resources/views/core/produit/menu.blade.php
@@ -6,14 +6,14 @@
 
         <div  id="my-menu">
             <a href="{{route('index_produit')}}"><button class="btn-contact100 btn-btn contact100-form-btn-white">Produits</button></a>
-            <a href="{{route('pannier')}}"><button class="btn-contact100 btn-btn">Mon Pannier</button></a>
+            <a href="{{route('pannier')}}"><button class="btn-contact100 btn-btn">Mon Panier</button></a>
             <a href="{{route('signin')}}"><button class="btn-contact100 btn-btn">Mon Compte</button></a>
         <a href="#" class="my-menu-humb"><i class="fa fa-bars" aria-hidden="true"></i></a>
         </div>
 
         <div  id="mobile-menu" class="display-switch-off">
             <a href="{{route('index_produit')}}"><button class="btn-contact100 btn-btn contact100-form-btn-white">Produits</button></a>
-            <a href="{{route('pannier')}}"><button class="btn-contact100 btn-btn">Mon Pannier</button></a>
+            <a href="{{route('pannier')}}"><button class="btn-contact100 btn-btn">Mon Panier</button></a>
             <a href="{{route('signin')}}"><button class="btn-contact100 btn-btn">Mon Compte</button></a>
         </div>
     </div>
