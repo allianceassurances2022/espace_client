392cb045149b7c0410aba882ec052446e4ff7fc7
AMINA BELMESSOUS <ABELMESSOUS@allianceassurances.com>
2020-07-16 10:57:34 +0100

    Initial commit

--- app/Http/Controllers/TarificationController.php
+++ app/Http/Controllers/TarificationController.php
@@ -9,7 +9,6 @@ use App\zcatnat;
 
 use App\Rsq_Immobilier;
 use App\devis;
-use App\Agences;
 
 use auth;
 
@@ -698,14 +697,13 @@ class TarificationController extends Controller
         $prime_total=$devis->prime_total;
         $wilaya_selected = $risque->code_wilaya;
         $wilaya = wilaya::all();
-        $agences = Agences::all();
         $adresse = $risque->adresse;
         $surface = $risque->superficie;
         $etage = $risque->etage;
 
             
 
-        return view('produits.mrh.devis_mrh',compact('terasse','habitation','montant','juredique','nbr_piece','prime_total','date_souscription','wilaya','date_eff','date_exp','adresse','wilaya_selected','surface','etage','id','agences'));
+        return view('produits.mrh.devis_mrh',compact('terasse','habitation','montant','juredique','nbr_piece','prime_total','date_souscription','wilaya','date_eff','date_exp','adresse','wilaya_selected','surface','etage','id'));
           
 
     }
