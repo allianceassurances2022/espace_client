20636ad55cf6ab1e4b65be53ff53677cc8ba4645
djilali <delmedjadji@allianceasurances.com.dz>
2021-09-06 10:37:07 +0100

    06-09-2021

--- app/Http/Controllers/APIs/GetPoliceAPI.php
+++ app/Http/Controllers/APIs/GetPoliceAPI.php
@@ -13,8 +13,8 @@ class GetPoliceAPI
     {
         $user_code_assure = $request->input('code'); //code assure
         $client = new \GuzzleHttp\Client();
-        //$url = "http://10.0.0.51:3000/getPolicesAssure?code=".$user_code_assure;
-        $url = "http://41.111.145.36/backoffice/public/api/get_police?code=".$user_code_assure;
+        $url = "http://41.111.145.36:3000/getPolicesAssure?code=".$user_code_assure;
+        //$url = "http://41.111.145.36/backoffice/public/api/get_police?code=".$user_code_assure;
         $data = Http::contentType("application/json")->send('GET',$url)->json();
 
         $data = json_encode($data);
